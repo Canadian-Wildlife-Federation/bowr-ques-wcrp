d5f02277044aa767e7e59e92e148285ad7abb4df
DanMckelvey <105823269+DanMckelvey@users.noreply.github.com>
2024-12-01 22:34:11 -0800

    changes to api_calls.py
    Changed api call to ELK watershed

--- content/python/api_calls.py
+++ content/python/api_calls.py
@@ -7,28 +7,39 @@ import pandas
 
 def barrier_extent(barrier_type):
 
-    request = 'https://cabd-pro.cwf-fcf.org/bcfishpass/functions/postgisftw.wcrp_barrier_extent/items.json?watershed_group_code=HORS&barrier_type=' + barrier_type
+    request1 = 'https://cabd-pro.cwf-fcf.org/bcfishpass/functions/postgisftw.wcrp_barrier_extent/items.json?watershed_group_code=BOWR&barrier_type=' + barrier_type
+    request2 = 'https://cabd-pro.cwf-fcf.org/bcfishpass/functions/postgisftw.wcrp_barrier_extent/items.json?watershed_group_code=QUES&barrier_type=' + barrier_type
 
-    response_api = requests.get(request)
+
+    response_api = requests.get(request1)
     parse = response_api.text
-    result = json.loads(parse)
+    result1 = json.loads(parse)
 
-    blocked_km = result[0]['all_habitat_blocked_km']
-    blocked_pct = result[0]['extent_pct']
+    response_api = requests.get(request2)
+    parse = response_api.text
+    result2 = json.loads(parse)
+
+    blocked_km = result1[0]['all_habitat_blocked_km'] + result2[0]['all_habitat_blocked_km']
+    blocked_pct = result1[0]['extent_pct'] + result2[0]['extent_pct']
 
     return blocked_km, blocked_pct
 
 def barrier_count(barrier_type):
-    request = 'https://cabd-pro.cwf-fcf.org/bcfishpass/functions/postgisftw.wcrp_barrier_count/items.json?watershed_group_code=HORS&barrier_type=' + barrier_type
+    request1 = 'https://cabd-pro.cwf-fcf.org/bcfishpass/functions/postgisftw.wcrp_barrier_count/items.json?watershed_group_code=BOWR&barrier_type=' + barrier_type
+    request2 = 'https://cabd-pro.cwf-fcf.org/bcfishpass/functions/postgisftw.wcrp_barrier_count/items.json?watershed_group_code=QUES&barrier_type=' + barrier_type
 
-    response_api = requests.get(request)
+    response_api = requests.get(request1)
     parse = response_api.text
-    result = json.loads(parse)
+    result1 = json.loads(parse)
 
-    n_passable = result[0]['n_passable']
-    n_barrier = result[0]['n_barrier']
-    n_potential = result[0]['n_potential']
-    n_unknown = result[0]['n_unknown']
+    response_api = requests.get(request2)
+    parse = response_api.text
+    result2 = json.loads(parse)
+
+    n_passable = result1[0]['n_passable'] + result2[0]['n_passable']
+    n_barrier = result1[0]['n_barrier'] + result2[0]['n_barrier']
+    n_potential = result1[0]['n_potential'] + result2[0]['n_potential']
+    n_unknown = result1[0]['n_unknown'] + result2[0]['n_unknown']
 
     sum_bar = (n_passable, n_barrier, n_potential, n_unknown)
 
@@ -36,36 +47,46 @@ def barrier_count(barrier_type):
 
 def barrier_severity(barrier_type):
 
-    request = 'https://cabd-pro.cwf-fcf.org/bcfishpass/functions/postgisftw.wcrp_barrier_severity/items.json?watershed_group_code=HORS&barrier_type=' + barrier_type
+    request1 = 'https://cabd-pro.cwf-fcf.org/bcfishpass/functions/postgisftw.wcrp_barrier_severity/items.json?watershed_group_code=BOWR&barrier_type=' + barrier_type
+    request2 = 'https://cabd-pro.cwf-fcf.org/bcfishpass/functions/postgisftw.wcrp_barrier_severity/items.json?watershed_group_code=QUES&barrier_type=' + barrier_type
 
-    response_api = requests.get(request)
+    response_api = requests.get(request1)
     parse = response_api.text
-    result = json.loads(parse)
+    result1 = json.loads(parse)
 
-    n_assessed_barrier = result[0]['n_assessed_barrier']
-    n_assess_total = result[0]['n_assess_total']
-    pct_assessed_barrier = result[0]['pct_assessed_barrier']
+    response_api = requests.get(request2)
+    parse = response_api.text
+    result2 = json.loads(parse)
+
+    n_assessed_barrier = result1[0]['n_assessed_barrier'] + result2[0]['n_assessed_barrier']
+    n_assess_total = result1[0]['n_assess_total'] + result2[0]['n_assess_total']
+    pct_assessed_barrier = result1[0]['pct_assessed_barrier'] + result2[0]['pct_assessed_barrier']
 
     return n_assessed_barrier, n_assess_total, pct_assessed_barrier
 
 def watershed_connectivity(habitat_type):
 
-    request = 'https://cabd-pro.cwf-fcf.org/bcfishpass/functions/postgisftw.wcrp_habitat_connectivity_status/items.json?watershed_group_code=HORS&habitat_type=' + habitat_type
+    request1 = 'https://cabd-pro.cwf-fcf.org/bcfishpass/functions/postgisftw.wcrp_habitat_connectivity_status/items.json?watershed_group_code=BOWR&habitat_type=' + habitat_type
+    request2 = 'https://cabd-pro.cwf-fcf.org/bcfishpass/functions/postgisftw.wcrp_habitat_connectivity_status/items.json?watershed_group_code=QUES&habitat_type=' + habitat_type
 
-    response_api = requests.get(request)
+    response_api = requests.get(request1)
     parse = response_api.text
-    result = json.loads(parse)
+    result1 = json.loads(parse)
 
-    connect_stat = result[0]['connectivity_status']
-    all_habitat = result[0]['all_habitat']
-    all_habitat_acc = result[0]['all_habitat_accessible']
+    response_api = requests.get(request2)
+    parse = response_api.text
+    result2 = json.loads(parse)
+
+    connect_stat = (result1[0]['connectivity_status'] + result2[0]['connectivity_status'])/2
+    all_habitat = result1[0]['all_habitat'] + result2[0]['all_habitat']
+    all_habitat_acc = result1[0]['all_habitat_accessible'] + result2[0]['all_habitat_accessible']
 
     return round(connect_stat), all_habitat, all_habitat_acc
 
 warnings.filterwarnings('ignore')
 
 connect = watershed_connectivity("ALL")[0]
-total = watershed_connectivity("ALL")[1] #total km in HORS
+total = watershed_connectivity("ALL")[1] #total km 
 access = watershed_connectivity("ALL")[2]
 gain = round((total*0.96)-access,2)
 
